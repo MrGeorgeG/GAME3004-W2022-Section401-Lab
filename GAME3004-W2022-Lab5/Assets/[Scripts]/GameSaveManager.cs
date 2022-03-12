f926826bc33b66d41ff7f635da3086b549d0ce35
Xinlin Gan <georgegan94@gmail.com>
2022-03-12 04:11:30 -0500

    Created Save-Load System with PlayerPrefs

---
+++ GAME3004-W2022-Lab5/Assets/[Scripts]/GameSaveManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSaveManager : MonoBehaviour
+{
+    public Transform player;
+    private void SaveGame()
+    {
+        PlayerPrefs.SetFloat("PlayerPositionX", player.position.x);
+        PlayerPrefs.SetFloat("PlayerPositionY", player.position.y);
+        PlayerPrefs.SetFloat("PlayerPositionZ", player.position.z);
+        PlayerPrefs.Save();
+        Debug.Log("Game data saved!");
+    }
+
+    private void LoadGame()
+    {
+        if (PlayerPrefs.HasKey("PlayerPositionX"))
+        {
+            var x = PlayerPrefs.GetFloat("PlayerPositionX");
+            var y = PlayerPrefs.GetFloat("PlayerPositionY");
+            var z = PlayerPrefs.GetFloat("PlayerPositionZ");
+
+            player.gameObject.GetComponent<CharacterController>().enabled = false;
+            player.position = new Vector3(x, y, z);
+            player.gameObject.GetComponent<CharacterController>().enabled = true;
+
+            Debug.Log("Game data loaded");
+        }
+        else
+        {
+            Debug.LogError("There is no save data!");
+
+        }
+    }
+
+    void ResetData()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Data reset complete");
+    }
+
+    public void OnSaveButton_Pressed()
+    {
+        SaveGame();
+    }
+
+    public void OnLoadButton_Pressed()
+    {
+        LoadGame();
+    }    
+}
